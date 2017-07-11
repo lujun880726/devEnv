b0f7328ac28181066413bf32393f23ac911be697
jun <jun.lu.726@gmail.com>
2017-07-11 14:25:05 +0800

    Create checkDaemon.sh
    判断文件下平的守护进程是否都存在，不在则重启

---
+++ checkDaemon.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export update_time=$(date +%Y%m%d%H%M%S)
+
+#Folder_A=/home/lujun/lujun.dev.jxch168.com/cli/Daemon  
+Folder_A=/home/wwwroot/m2.jxch168.com/cli/Daemon  
+for file_a in ${Folder_A}/*; do  
+    temp_file=`basename $file_a`  
+    count=`ps -ef |grep $temp_file |grep -v "grep" |wc -l`
+    if [ 0 == $count ];then
+        nohup php ${Folder_A}/${temp_file} >> /tmp/${temp_file%.*}.out &
+
+	echo [${update_time}] $temp_file ' is run!'
+    else
+	echo [${update_time}] $temp_file ' is there!'
+    fi
+done
