0d90858efffeb9710ead42ecfbd88b488eae9483
jun <jun.lu.726@gmail.com>
2019-09-16 09:44:34 +0800

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -1,11 +1,4 @@
 rm -rf /home/wwwlogs/*
-rm -rf /home/csm/wwwlogs/*
-rm -rf /home/dch/wwwlogs/*
 rm -rf /home/lujun/wwwlogs/*
-rm -rf /home/lrj/wwwlogs/*
-rm -rf /home/wangjun/wwwlogs/*
-rm -rf /home/wbt/wwwlogs/*
-rm -rf /home/xmc/wwwlogs/*
-rm -rf /home/zhw/wwwlogs/*
 rm -rf /data/wwwlogs/*.log
 /etc/init.d/nginx restart
