760930c7fa86ff7cf5259f65feffb4576de67f4f
jun <jun.lu.726@gmail.com>
2020-09-02 15:34:18 +0800

    Update dumpMysqlAllTableDataLimit.php

--- dumpMysqlAllTableDataLimit.php
+++ dumpMysqlAllTableDataLimit.php
@@ -15,8 +15,14 @@ if ($con) {
         foreach ($info1 as $val) {
 
             $result = [];
-            $_tmpRes = @mysqli_query($con, "select * from `" . $val . "` limit 5000");
-            if (!$_tmpRes) die("SQL(fetchRow):{$sql} " . mysqli_error($con));
+            if(in_array($val,['role_user','report_test_copy','report_test','admin_role','admin_user','adx_ads','config_common','config_ip2location','migrations','password_resets','permission_role']))
+            {
+                $_tmpRes = @mysqli_query($con, "select * from `" . $val . "`  limit 3000");
+            }else{
+                $_tmpRes = @mysqli_query($con, "select * from `" . $val . "` order by id desc  limit 3000");
+            }
+
+            if (!$_tmpRes) die("SQL(fetchRow):{$val}" . mysqli_error($con));
             while ($res = mysqli_fetch_assoc($_tmpRes)) {
                 $result[] = $res;
             }
@@ -25,16 +31,27 @@ if ($con) {
             $tmp_key = "";
             if (!empty($result)) {
                 foreach ($result as $key => $value) {
+                    $sql = '';
                     $tmp_key = '(`' . implode('`,`', array_keys($value)) . '`) values ';
-                    $tmp_value .= "('" . implode("','", array_values($value)) . "'),";
+                    $tmp_value = '("' . implode('","', array_values(real_escape_string_byArr($value,$con))) . '");';
+                    $sql = "insert into `" . $val . "`" . $tmp_key . $tmp_value;
+                    $sql = substr($sql, 0, -1) . ";\r\n";
+                    file_put_contents("./dump_" . $dbname . ".sql", $sql, FILE_APPEND);
+
                 }
-                $sql = "insert into `" . $val . "`" . $tmp_key . $tmp_value;
-                $sql = substr($sql, 0, -1) . ";\r\n";
+
             }
-            file_put_contents("./dump_" . $dbname . ".sql", $sql, FILE_APPEND);
             var_dump($val);
         }
     }
+}
+
+function real_escape_string_byArr($arr,$con)
+{
+    foreach ($arr as $Key => $val) {
+        $arr[$Key] = mysqli_real_escape_string($con,$val);
+    }
+    return $arr;
 }
 
 echo "END!";
